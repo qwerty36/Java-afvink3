09ca9abd18a6e995bb1f1c12b370f3d912add435
Richard Jansen <richard@lrtbl.nl>
2015-10-01 20:29:56 +0200

    megweh

---
+++ afvink3/src/afvink3.java
@@ -0,0 +1,13 @@
+public class afvink3 {
+    private String naam;
+    private int nummer;
+
+    public afvink3(String naam, int nummer){
+        this.naam = naam;
+        this.nummer = nummer;
+    }
+    public afvink3(String studnaam){naam = studnaam;}
+    public afvink3(int studnummer) {nummer = studnummer;}
+    
+
+}
