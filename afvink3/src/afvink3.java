1ed28e3744608a586fe1f432cdb7832fc56bed42
Richard Jansen <richard@lrtbl.nl>
2015-10-07 11:03:24 +0200

    fuck you NSA

--- afvink3/src/afvink3.java
+++ afvink3/src/afvink3.java
@@ -1,13 +1,40 @@
+/**
+ * Created by richard on 10/7/15.
+ */
 public class afvink3 {
     private String naam;
     private int nummer;
 
-    public afvink3(String naam, int nummer){
+    public afvink3(String naam, int nummer) {
         this.naam = naam;
         this.nummer = nummer;
     }
-    public afvink3(String studnaam){naam = studnaam;}
-    public afvink3(int studnummer) {nummer = studnummer;}
-    
 
+    public afvink3(String studnaam) {
+        naam = studnaam;
+    }
+
+    public afvink3(int studnummer) {
+        nummer = studnummer;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public int getNummer() {
+        return nummer;
+    }
+
+    public void setNummer(int nummer) {
+        this.nummer = nummer;
+    }
+
+    public void printInfo() {
+        System.out.println("Helleuw " + getNaam() + " Je nummer is " + getNummer());
+    }
 }
---
+++ afvink3/src/afvink3_test.java
@@ -0,0 +1,15 @@
+/**
+ * Created by richard on 10/7/15.
+ */
+public class afvink3_test {
+    public static void main (String[] args){
+        afvink3 s1= new afvink3("Richard",123456);
+        s1.printInfo();
+        afvink3 s2= new afvink3("Obama",133769);
+        s2.printInfo();
+        afvink3 s3= new afvink3("Pijl de Pinda",9001420);
+        s3.printInfo();
+        afvink3 s4= new afvink3("Mart1nus",32342342);
+        s4.printInfo();
+    }
+}
